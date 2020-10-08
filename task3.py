e562e00460bedcea19f5be782c5fbba88cc8d46e
jeffkim0627 <55156837+jeffkim0627@users.noreply.github.com>
2020-10-08 13:05:13 -0700

    update

--- task3.py
+++ task3.py
@@ -6,3 +6,6 @@ The return value is the sum of all the numbers in the list
 added together
 (2 points)
 """
+def perimeter(list):
+    x = sum[]
+    return x
--- task4.py
+++ task4.py
@@ -6,4 +6,10 @@ Return True if the number is an integer
 Return False if the number is not an integer
 (2 points)
 """
+import math
 
+def isItnteger(number):
+    if (number/2) == (math.ceil(number/2)):
+        return True
+    else:
+        return False
