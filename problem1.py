d97ecc39542531e648858801ec3e3980409b34b9
jeffkim0627 <55156837+jeffkim0627@users.noreply.github.com>
2020-11-16 13:05:56 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -7,3 +7,14 @@ If the boolean is False, then the larger number is the hypotenuse
 Return the missing side
 (2 points)
 """
+def hypotenuse():
+    if boolean == True:
+        hyp = (a**2 + b**2)**(1/2)
+        return hyp
+    else:
+        if a>b:
+            missingside = (a**2 - b**2)**(1/2)
+        else:
+            b
+            missingside = (b**2 - a**2)**(1/2)
+        return missingside
